806b17d83c19a1048139a455a4fd07072c29ad6a
Scyth3-c <kevintor2246@gmail.com>
2022-01-24 21:55:21 -0500

    assembly added

--- compiler.js
+++ compiler.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const fs = require('fs');
 const state = {
     time: {
         hour: new Date().getHours(),
@@ -9,13 +9,10 @@ const state = {
     seed: Math.floor(Math.random()*(12323-102)) + 10,
     standar: "c++17",
     optimizar: "1",
-    API: "   "
+    API: "https://radiant-dusk-88409.herokuapp.com/"
 };
-
-
 const compile = (code)=> new Promise(async (resolve,reject)=>{
     console.log("iniciando...");
-
     const res = await axios.post(`${state.API}addon/compile`, code , {
         headers: {
           "Content-Type": "text/plain",
@@ -26,7 +23,6 @@ const compile = (code)=> new Promise(async (resolve,reject)=>{
           o: state.optimizar,
         },
       });
-      
       let output = res.data.toString();
       if(output.length >= 256) {
         resolve("salida extensa (error o no) para mostrar, revise y vuelva a compilar")
@@ -37,5 +33,31 @@ const compile = (code)=> new Promise(async (resolve,reject)=>{
       }
 
 });
-
+const assembly = (code, message) => new Promise(async (resolve,reject)=>{
+  const res = await axios.post(`${state.API}addon/assembly`, code , {
+    headers: {
+      "Content-Type": "text/plain",
+      title: `temp_file_${state.seed}_${(state.time.hour,
+      state.time.min,
+      state.time.sec)}`,
+      standar: state.standar,
+      o: state.optimizar,
+    },
+  });
+   const title = `./temp/${state.time.sec}${state.time.min}file.asm`;
+    let stream = fs.createWriteStream(title, { encoding: "utf-8" });
+    stream.write(res.data);
+    stream.end();
+    stream.addListener("close", async () => {
+        try {
+          await message.channel.send({
+            files: [title]
+          });
+        resolve(title);
+      } catch (error) {
+        resolve(false);
+      }
+    });
+}); 
+exports.assembly = assembly;
 exports.compile = compile;
